b16f7d9372c12fe61cd80be775b84f8150111eec
Marc Satkowski <marc.satkowski@tu-dresden.de>
2014-11-22 14:21:56 +0100

    Praktika_1-2: Aufbau des Fenster und hinzufügen von Methoden.
    TODO:
    - Berechnungen herausfinden
    - Berechnungen implementieren

--- openCV-Praktika1-2/main.cpp
+++ openCV-Praktika1-2/main.cpp
@@ -1,1 +1,39 @@
+#include <main.h>
 
+int main( int argc, const char** argv ) {
+    Vec3i spherCoord (0,0,0);
+    Vec3i affineTrans (0,0,0);
+    Vec3i camPos;
+    Vec<void*, 2> data (&spherCoord, &camPos);
+
+    //Creating the main window
+    namedWindow("Image", 0);
+
+    //Adding the trackbars for the spherical coordinates
+    createTrackbar("radius", "Image", &(spherCoord.val[0]), 100, calcCameraPosition, &data);
+    createTrackbar("theta ", "Image", &(spherCoord.val[1]), 100, calcCameraPosition, &data);
+    createTrackbar("rho   ", "Image", &(spherCoord.val[2]), 100, calcCameraPosition, &data);
+
+    //Adding the trackbars for the affine transformation
+    createTrackbar("alpha", "Image", &(affineTrans.val[0]), 100, calcAffineTransformation, &affineTrans);
+    createTrackbar("beta ", "Image", &(affineTrans.val[1]), 100, calcAffineTransformation, &affineTrans);
+    createTrackbar("lamda", "Image", &(affineTrans.val[2]), 100, calcAffineTransformation, &affineTrans);
+
+    waitKey();
+    return 0;
+}
+
+static void calcCameraPosition(int, void* userdata) {
+    Vec<void*, 2> data;
+    Vec3i* spherCoord;
+    Vec3i* camPos;
+
+    //Cast of the userdata
+    data = static_cast<Vec<void*, 2> >(userdata);
+    spherCoord = static_cast<Vec3i*>(data.val[0]);
+    camPos = static_cast<Vec3i*>(data.val[1]);
+}
+
+static void calcAffineTransformation(int, void* userdata) {
+
+}
--- openCV-Praktika1-2/main.h
+++ openCV-Praktika1-2/main.h
@@ -1,4 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+
+#include <stdio.h>
+
+using namespace cv;
+
+//Method that calculate the camera position
+static void calcCameraPosition(int, void* userdata);
+//Method that calculate the affine transformation
+static void calcAffineTransformation(int, void* userdata);
+
+//Main method
+int main( int argc, const char** argv );
+
 #endif // MAIN_H
