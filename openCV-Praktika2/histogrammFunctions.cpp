e81afabe390f89d3fc4a0dd8787e99507d6c5464
Marc Satkowski <marc.satkowski@tu-dresden.de>
2014-12-11 12:27:33 +0100

    Homework_2: Kommentare erweitert.
    TODO:
    - Aufgabe 1.1, 1.2, 1.2 implementieren
    - Aufgabe 2b implementieren
    - Aufgabe 3 implementieren
    - Aufgabe 4 implementieren

--- openCV-Praktika2/histogrammFunctions.cpp
+++ openCV-Praktika2/histogrammFunctions.cpp
@@ -9,12 +9,15 @@ vector<Mat> calcCDFandPDF(Mat *input, bool convert) {
     float range[2] = { 0, 255 };
     const float* ranges[1];
     /*-----------------------------------------*/
+    //Is it necessary to convert the input
     if(convert)
         cvtColor(*input, tempInput, CV_BGR2GRAY);
+    else
+       tempInput = *input;
 
+    //Set values
     histSize = 256;
     *ranges = { range };
-
     histWidth = 512;
     histHeight = 400;
     binWidth = cvRound((double) histWidth/histSize);
@@ -22,7 +25,7 @@ vector<Mat> calcCDFandPDF(Mat *input, bool convert) {
     pdfImage = Mat(histHeight, histWidth, CV_8U, Scalar(0, 0, 0));
     cdfImage = Mat(histHeight, histWidth, CV_8U, Scalar(0, 0, 0));
 
-    //Calculate histogram
+    //Calculate pdf
     calcHist(&tempInput, 1, 0, Mat(), pdf, 1, &histSize, ranges, true, false);
     //Plot the pdf
     normalize(pdf, pdf, 0, pdfImage.rows, NORM_MINMAX, -1, Mat());
@@ -35,6 +38,7 @@ vector<Mat> calcCDFandPDF(Mat *input, bool convert) {
 
     cdf = Mat(pdf.rows, pdf.cols, CV_32F);
 
+    //Calculate cdf
     cdf.at<float>(0) = pdf.at<float>(0);
     for(int cY = 1; cY < cdf.rows; cY++)
         cdf.at<float>(cY) = cdf.at<float>(cY - 1) + pdf.at<float>(cY);
@@ -48,6 +52,7 @@ vector<Mat> calcCDFandPDF(Mat *input, bool convert) {
                        Scalar(255, 0, 0),
                        2, 8, 0);
 
+    //Create the vector and return it
     return vector<Mat> { pdf, cdf, pdfImage, cdfImage };
 }
 
@@ -65,6 +70,7 @@ vector<Mat> histogrammEqualizationOneChannel(Mat *input) {
         printf("Cannot read the image %s\n", filename.c_str());
     }
 
+    //Calculate the cdf, pdf and the responding graphs
     image1 = calcCDFandPDF(&firstImage, true);
     image2 = calcCDFandPDF(input, true);
     cdf1 = image1.at(1);
@@ -72,22 +78,26 @@ vector<Mat> histogrammEqualizationOneChannel(Mat *input) {
     lut = Mat(cdf1.rows, 1 , CV_8U);
     cdfOutputImage = Mat(firstImage.rows, firstImage.cols, CV_8U);
 
+    //Histogram matching of both cdf
     for(int c1 = 0; c1 < cdf1.rows; c1++) {
         for(int c2 = 0; c2 < cdf2.rows; c2++) {
+            //A match if the cdf1 value is bigger than the cdf2 value or its the last value in cdf2
             if(cdf1.at<float>(c1) > cdf2.at<float>(c2) && (c2 + 1) != cdf2.rows) continue;
             lut.at<uchar>(c1) = c2;
             break;
         }
     }
 
+    //Calculate the pdf image
     cvtColor(firstImage, firstImage, CV_BGR2GRAY);
     equalizeHist(firstImage, pdfOutputImage);
 
+    //Calculate the cdf image
     for(int cY = 0; cY < cdfOutputImage.rows; cY++)
         for(int cX = 0; cX < cdfOutputImage.cols; cX++)
             cdfOutputImage.at<uchar>(cY, cX) = lut.at<uchar>(firstImage.at<uchar>(cY, cX));
 
-
+    //Create the vector and return it
     return vector<Mat> { image1.at(3), image2.at(3), firstImage, pdfOutputImage, cdfOutputImage };
 }
 
@@ -101,15 +111,18 @@ vector<Mat> histogrammEqualizationThreeChannel(Mat *input) {
     Mat cdf1[3], cdf2[3], lut[3];
     bool lutSet[3];
     /*-----------------------------------------*/
+
     //Creating the image and testing if it is empty or not
     firstImage = imread(filename);
     while(firstImage.empty()) {
         printf("Cannot read the image %s\n", filename.c_str());
     }
 
+    //Split the different color channel
     split(firstImage, image1Split);
     split(*input, image2Split);
 
+    //Calculate the cdf, pdf and the responding graphs for all channel
     for(int c = 0; c < 3; c++) {
         image1[c] = calcCDFandPDF(&image1Split[c], false);
         image2[c] = calcCDFandPDF(&image2Split[c], false);
@@ -119,12 +132,14 @@ vector<Mat> histogrammEqualizationThreeChannel(Mat *input) {
     }
     cdfOutputImage = Mat(firstImage.rows, firstImage.cols, CV_8UC3);
 
+    //Histogram matching of both cdf for all channel
     for(int c1 = 0; c1 < cdf1[0].rows; c1++) {
         lutSet[0] = false;
         lutSet[1] = false;
         lutSet[2] = false;
         for(int c2 = 0; c2 < cdf2[0].rows; c2++) {
             for(int channel = 0; channel < 3; channel++) {
+                //A match if the cdf1 value is bigger than the cdf2 value or its the last value in cdf2
                 if((cdf1[channel].at<float>(c1) <= cdf2[channel].at<float>(c2) || (c2 + 1) == cdf2[channel].rows)
                     && !lutSet[channel]) {
                     lut[channel].at<uchar>(c1) = c2;
@@ -134,13 +149,14 @@ vector<Mat> histogrammEqualizationThreeChannel(Mat *input) {
             if(lutSet[0] && lutSet[1] && lutSet[2]) break;
         }
     }
-
+    //Calculate the cdf image
     for(int cY = 0; cY < cdfOutputImage.rows; cY++)
         for(int cX = 0; cX < cdfOutputImage.cols; cX++)
             cdfOutputImage.at<Vec3b>(cY, cX) = Vec3b { lut[0].at<uchar>(firstImage.at<Vec3b>(cY, cX).val[0]),
                                                        lut[1].at<uchar>(firstImage.at<Vec3b>(cY, cX).val[1]),
                                                        lut[2].at<uchar>(firstImage.at<Vec3b>(cY, cX).val[2]) };
 
+    //Create the vector and return it
     return vector<Mat> { image1[0].at(3), image2[0].at(3),
                          image1[1].at(3), image2[1].at(3),
                          image1[2].at(3), image2[2].at(3),
